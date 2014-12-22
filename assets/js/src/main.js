506260ea44c17e4351c2f38d3204e801531c9d0c
Tom Lagier <tom@rosetta.com>
2014-12-22 12:19:12 -0800

    Moving to JS init

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -2,57 +2,57 @@
 (function (window, document, $, undefined) {
   'use strict';
 
-  // var VideoIn = function () {
+  var VideoIn = function () {
 
-  //   //jQuery selectors
-  //   var $body = $('body');
-  //   var $video = $body.find('#input-video');
-  //   var $button = $body.find('#load-video');
+    //jQuery selectors
+    var $body = $('body');
+    var $video = $body.find('#input-video');
+    var $button = $body.find('#load-video');
 
-  //   this.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.oGetUserMedia || navigator.msGetUserMedia;
+    this.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.oGetUserMedia || navigator.msGetUserMedia;
 
-  //   this.hasUserMedia = function () {
-  //     return !!this.getUserMedia;
-  //   };
+    this.hasUserMedia = function () {
+      return !!this.getUserMedia;
+    };
 
-  //   this.errorCallback = function (e) {
-  //     console.log('Error: ', e);
-  //   };
+    this.errorCallback = function (e) {
+      console.log('Error: ', e);
+    };
 
-  //   this.init = function () {
-  //     if (this.hasUserMedia) {
-  //       $button.click(function () {
-  //         this.loadUserMedia();
-  //       }.bind(this));
-  //     } else {
-  //       console.log('getUserMedia() is not supported in your browser');
-  //     }
-  //   };
+    this.init = function () {
+      if (this.hasUserMedia) {
+        $button.click(function () {
+          this.loadUserMedia();
+        }.bind(this));
+      } else {
+        console.log('getUserMedia() is not supported in your browser');
+      }
+    };
 
-  //   this.loadUserMedia = function () {
-  //     var settings = {
-  //       video: true,
-  //       audio: true,
-  //     };
+    this.loadUserMedia = function () {
+      var settings = {
+        video: true,
+        audio: true,
+      };
 
-  //     this.getUserMedia.call(navigator, settings, this.displayVideo, this.errorCallback);
+      this.getUserMedia.call(navigator, settings, this.displayVideo, this.errorCallback);
 
-  //   };
+    };
 
-  //   this.displayVideo = function (userVideoStream) {
-  //     $video.attr('src', window.URL.createObjectURL(userVideoStream));
-  //     $video.on('loadedmetadata', function (e) {
-  //       console.log(e);
-  //     });
-  //   };
-  // };
+    this.displayVideo = function (userVideoStream) {
+      $video.attr('src', window.URL.createObjectURL(userVideoStream));
+      $video.on('loadedmetadata', function (e) {
+        console.log(e);
+      });
+    };
+  };
 
   var VideoOut = function () {
 
-    // this.src = {
-    //   src: 'rtmp://ec2-54-149-64-14.us-west-2.compute.amazonaws.com/live/test.flv',
-    //   type: 'rtmp/flv'
-    // };
+    this.src = {
+      src: 'rtmp://ec2-54-149-64-14.us-west-2.compute.amazonaws.com/live/test',
+      type: 'rtmp/flv'
+    };
 
     this.init = function () {
       // jwplayer('video-out').setup({
@@ -61,23 +61,26 @@
       //   width: 600
       // });'
 
-      // var options = {
-      //   autoplay: true,
-      //   src: this.src
-      // };
+      var options = {
+        autoplay: true,
+        src: this.src,
+        preload: 'auto',
+        techOrder: ['html5', 'flash'],
+        controls: true,
+      };
 
-      //videojs('video-out', options);
+      videojs('video-out', options);
 
     };
 
   };
 
   $(function () {
-    // var videoIn = videoIn || new VideoIn();
-    // videoIn.init();
+    var videoIn = videoIn || new VideoIn();
+    videoIn.init();
 
-    // var videoOut = videoOut || new VideoOut();
-    // videoOut.init();
+    var videoOut = videoOut || new VideoOut();
+    videoOut.init();
   });
 
 })(window, document, jQuery);
