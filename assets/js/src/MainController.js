4807421036a377ed7c93510792f1b8eb4823d3d5
Tom Lagier <tom@rosetta.com>
2014-12-23 12:51:42 -0800

    Reworked closure in MainController

--- assets/js/src/MainController.js
+++ assets/js/src/MainController.js
@@ -1,24 +1,26 @@
 /* globals _, ChatRoom */
-var MainController = (function (window, $, _, undefined) {
+(function (window, $, _, undefined) {
   'use strict';
 
-  return {
-    init: function () {
-      var options = {
-        serverAddress: 'http://ec2-54-149-64-14.us-west-2.compute.amazonaws.com:8081',
-      };
+  var MainController = function () {
 
-      this.chatRoom = new ChatRoom(options);
-    }
+    return {
+      init: function () {
+        var options = {
+          serverAddress: 'http://ec2-54-149-64-14.us-west-2.compute.amazonaws.com:8081',
+        };
+
+        this.chatRoom = new ChatRoom(options);
+      }
+    };
   };
+
+
+  $(function () {
+    var mainController = new MainController();
+    mainController.init();
+
+    window.mainController = mainController;
+  });
+
 })(window, $, _);
-
-
-$(function () {
-  'use strict';
-
-  var mainController = new MainController();
-  mainController.init();
-
-  window.mainController = mainController;
-});
