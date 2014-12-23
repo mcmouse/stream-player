bc58bf2fe06795fb62f396ea5dcd5cd3136933ee
Tom Lagier <tom@rosetta.com>
2014-12-23 10:41:01 -0800

    Removed IFEE from server.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
 /* globals require */
-(function () {
-  'use strict';
 
-  var app = require('express')();
-  var server = require('http').Server(app);
-  var io = require('socket.io')(server);
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-  server.listen(8081);
+server.listen(8081);
 
-  io.on('connection', function (socket) {
-    socket.emit('news', {
-      hello: 'world'
-    });
-    socket.on('my other event', function (data) {
-      console.log(data);
-    });
+io.on('connection', function (socket) {
+  socket.emit('news', {
+    hello: 'world'
   });
-})();
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
