7bc0e7b6b8dba82909d10ba2fe77a1a22938ec0e
Tom Lagier <tom@rosetta.com>
2015-01-02 13:21:33 -0800

    Enter user name selector name fix

--- assets/js/src/views/Login.js
+++ assets/js/src/views/Login.js
@@ -37,7 +37,7 @@ var LoginView = (function () {
     },
 
     showEnterUserName: function () {
-      $(this.ui.enterUserName).show();
+      $(this.ui.enterUserNameRegion).show();
     },
 
     setName: function () {
