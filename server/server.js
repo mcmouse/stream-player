88c461bb4f4331f57434bb1a74bb153cd96984cf
Tom Lagier <tom@rosetta.com>
2015-01-02 11:40:12 -0800

    Socket.io syntax fix

--- server/server.js
+++ server/server.js
@@ -13,20 +13,21 @@
 
   chat.on('connection', function (socket) {
     console.log('user joined!');
+
+    socket.on('userJoined', function (data) {
+      console.log('broadcasting new user with name ' + data.username);
+      socket.emit('userJoined', data);
+    });
+
+    socket.on('userLeft', function (data) {
+      socket.emit('userLeft', data);
+    });
+
+    socket.on('newMessage', function (data) {
+      socket.emit('newMessage', data);
+    });
   });
 
-  chat.on('userJoined', function (data) {
-    console.log('broadcasting new user with name ' + data.username);
-    chat.emit('userJoined', data);
-  });
-
-  chat.on('userLeft', function (data) {
-    chat.emit('userLeft', data);
-  });
-
-  chat.on('newMessage', function (data) {
-    chat.emit('newMessage', data);
-  });
 
   webcams.on('connection', function (data) {
     console.log(data);
