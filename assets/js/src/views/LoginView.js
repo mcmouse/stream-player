0ff71e83e151e81d3ce209b2b0a040857eae68a8
Tom Lagier <tom@rosetta.com>
2015-01-05 16:16:44 -0800

    More template work

--- assets/js/src/views/LoginView.js
+++ assets/js/src/views/LoginView.js
@@ -20,7 +20,7 @@ define([
     //Static HTML
     template: function (model) {
       return _.template(LoginRegionTemplate)({
-        name: (model.currentUser && model.currentUser.get('name') ? model.currentUser.name : '')
+        name: (typeof model.currentUser !== 'undefined' && typeof model.currentUser.get('name') !== 'undefined' ? model.currentUser.get('name') : '')
       });
     },
 
