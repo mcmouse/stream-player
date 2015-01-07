2af53c28e781f86cd4d8c742aca7e9afd84ad232
Tom Lagier <tom@rosetta.com>
2015-01-07 14:09:06 -0800

    Added Marionette Radio and shim

--- assets/js/start.js
+++ assets/js/start.js
@@ -22,27 +22,54 @@ require.config({
       ],
       exports: 'Backbone.LocalStorage',
     },
-    'marionette': {
+    'backbone.babysitter': {
       deps: [
         'backbone'
       ],
+      exports: 'Backbone.BabySitter'
+    },
+    'backbone.radio': {
+      deps: [
+        'backbone'
+      ],
+      exports: 'Backbone.Radio'
+    },
+    'marionette': {
+      deps: [
+        'backbone',
+        'backbone.babysitter',
+        'backbone.radio',
+      ],
       exports: 'Marionette'
     },
+    'marionette.radio.shim': {
+      deps: [
+        'marionette'
+      ],
+    }
+  },
+  map: {
+    '*': {
+      'backbone.wreqr': 'backbone.radio'
+    }
   },
   paths: {
     jquery: 'https://code.jquery.com/jquery-2.1.3',
     socketio: 'https://cdn.socket.io/socket.io-1.2.1',
     underscore: '../libs/underscore',
     marionette: '../libs/marionette',
+    'marionette.radio.shim': '../libs/marionette-radio-shim',
     backbone: '../libs/backbone',
     'backbone.localstorage': '../libs/backbone-localstorage',
+    'backbone.radio': '../libs/backbone-radio',
+    'backbone.babysitter': '../libs/backbone/babysitter',
     templates: '../../templates',
     text: '../libs/text',
   }
 });
 
 require([
-  'ChatApp'
+  'ChatApp', 'marionette', 'marionette.radio.shim',
 ], function (ChatApp) {
   'use strict';
 
